7546eebbef0580be7bcbb07b9002e34a4d06e98c
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-14 15:20:23 +0900

    update

--- rankB/8.bazz.rb
+++ rankB/8.bazz.rb
@@ -1,17 +1,35 @@
-INPUT = <<~"EOS"
-2 5 3 1000
-100 100
-500 200
-60 300
-100 500
-10 1000
+# 入力テスト
+input = <<~EOS
+  4 4 4 12
+  1 1 1 1
+  1 2 3 4
+  1 3 6 10
+  1 4 10 20
 EOS
 
 def solve(input)
   data = input.split("\n")
   count, time, period, action_count = data.shift.split.map(&:to_i)
-  datas = Array.new(time){Array.new(count, 0)}
 
+  # 行動データを2次元配列に変換
+  actions = data.map { |line| line.split.map(&:to_i) }.transpose
+
+  answers = actions.map do |action|
+    # 各行動の期間内の合計を計算
+    total_actions = action.each_cons(period).map(&:sum)
+
+    # 条件を満たす最初のインデックスを探す
+    index = total_actions.find_index { |sum| sum >= action_count }
+    # 結果を設定
+    if index
+      ["yes", index + period]
+    else
+      ["no", nil]
+    end
+  end
+
+  # 結果を出力
+  answers.each { |result, index| puts "#{result} #{index}" }
 end
 
-solve(INPUT)
+solve(input)
